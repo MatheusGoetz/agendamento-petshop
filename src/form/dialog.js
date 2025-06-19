e30cec1076b00abe5e1e6ea6e2d940e18f9a5a32
Matheus Goetz <goetz.dev@gmail.com>
2025-06-19 17:54:48 -0300

    Finalizado criação do modal e estilização

---
+++ src/form/dialog.js
@@ -0,0 +1,11 @@
+const button = document.getElementById("new-schedule");
+const modal = document.querySelector("dialog");
+const schedule = document.getElementById("schedule");
+
+button.onclick = function(){
+  modal.showModal();
+}
+
+schedule.onclick = function(){
+  modal.closeModal();
+}
